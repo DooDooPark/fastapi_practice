1639cc32bbe05f1b1314f1d0dbaa463dce9a77a7
박두현 <doubled4017@gmail.com>
2023-11-20 16:33:39 +0900

    api practice connect db

--- planner/models/events.py
+++ planner/models/events.py
@@ -1,16 +1,18 @@
-from pydantic import BaseModel
 from typing import List
+
+from pydantic import BaseModel
+
 
 class Event(BaseModel):
     id: int
-    title:str
-    image:str
-    description:str
-    tags:List[str]
-    location:str
+    title: str
+    image: str
+    description: str
+    tags: List[str]
+    location: str
 
     class Config:
-        json_schema_extra = {
+        schema_extra = {
             "example": {
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
